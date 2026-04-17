c63c95e512d1968ffda8c93cee453f578d48a643
williamonweb <139515116+williamonweb@users.noreply.github.com>
2026-04-17 14:49:14 -0300

    Delete api directory

--- api/config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).json({
-    supabaseUrl: 'https://banpsoevrpyhncgjhglz.supabase.co',
-    supabaseAnonKey: 'sb_publishable_ZtXkeXO1wGAv5iauqIGY2Q_KYyftrZe',
-  });
-}
